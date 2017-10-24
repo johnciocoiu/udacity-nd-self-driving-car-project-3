83a06471ca7438e9dc733bce137b5a1a1590dd5b
John Ciocoiu <john.ciocoiu@kpn.com>
2017-10-24 12:28:13 +0200

    Add comments.

--- model.py
+++ model.py
@@ -5,14 +5,20 @@ import os
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, MaxPooling2D, Dropout
 
-def process_image(img):
-    return img
-
+# Save location of data files, so this data does not have to be generated over and over again when chaging the network.
 X_file = 'data/X_file.npy'
 y_file = 'data/y_file.npy'
 
-data_folders = ['data/internet/']
+# The folders to read the data from, multiple drivings around the track
+# internet = from udacity
+# track1 = driving mouse on track 1
+# track1_2 = driving mouse and recovering from outside the track
+# track1_reverse = driving mouse track 1 the other way around
+# training = driving with keyboard
+# track2 = single lap on track 2 with mouse
+data_folders = ['data/internet/', 'data/track1/', 'data/track1_2/', 'data/track1_reverse/', 'data/training/', 'data/track2/']
 
+# If file exists, load them, otherwise generate them
 if os.path.exists(X_file) and os.path.exists(y_file):
     X_train = np.load(X_file)
     y_train = np.load(y_file)
@@ -23,8 +29,9 @@ else:
     car_images = []
     steering_angles = []
 
+    # For every folder, add the data to the arrays
     for fnum, folder in enumerate(data_folders):
-        print "Doing folder ", fnum+1, "/", len(data_folders)
+        print "Doing folder ", fnum+1, "/", len(data_folders),
         print ""
         rows = []
         with open(folder+'driving_log.csv') as csvfile:
@@ -34,16 +41,18 @@ else:
 
         first = True
 
+        # Add a steering correction
         steering_correction = 0.2
-
         for j, row in enumerate(rows):
-            # print "(", j+1, "/", len(rows), ")", round((j+1)/len(rows)*100,2), "% completed..."
-            
+            # Print the current finishing of the current folder
+            print "(", j+1, "/", len(rows), ")", round((j+1)/len(rows)*100,2), "% completed...",
+        
             # Skip header row
             if first:
                 first = False
                 continue
                 
+            # For every of the 3 generated images, do the data generate thing
             for i in range(3):
                 steering_center = float(row[3])
 
@@ -52,9 +61,12 @@ else:
 
                 path = folder+"/IMG/"
                 img_loc = row[i].split('/')[-1]
-                img = process_image(cv2.imread(path + img_loc))
+                img = cv2.imread(path + img_loc)
 
+                # Add images to array
                 car_images.append(img)
+
+                # Add flipped images to array
                 car_images.append(cv2.flip(img, 1))
                 
                 angle = 0
@@ -68,19 +80,24 @@ else:
                 else:
                     raise Exception('Uhhhm?')
                 
+                # Add steering angle to array
                 steering_angles.append(angle)
+
+                # Add flipped steering angle to array
                 steering_angles.append(-angle)
 
+    # Generate np arrays
     X_train = np.array(car_images)
     y_train = np.array(steering_angles)
 
+    # Save the arrays for reuse options
     np.save(X_file, X_train)
     np.save(y_file, y_train)
 
     print ""
     print "Done making training set!" 
 
-
+# The model, based on https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf given in the lecture
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((60,20), (0,0))))
@@ -98,6 +115,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=6)
+history = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
+# Save the model to a file
 model.save('model.h5')
