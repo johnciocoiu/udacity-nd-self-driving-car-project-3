8896943700aa0f63aa392f3a8d853370ffdb0b8a
John Ciocoiu <john.ciocoiu@kpn.com>
2017-10-24 13:31:26 +0200

    Remove save, too large dataset.

--- model.py
+++ model.py
@@ -18,84 +18,72 @@ y_file = 'data/y_file.npy'
 # track2 = single lap on track 2 with mouse
 data_folders = ['data/internet/', 'data/track1/', 'data/track1_2/', 'data/track1_reverse/', 'data/training/', 'data/track2/']
 
-# If file exists, load them, otherwise generate them
-if os.path.exists(X_file) and os.path.exists(y_file):
-    X_train = np.load(X_file)
-    y_train = np.load(y_file)
+car_images = []
+steering_angles = []
 
+# For every folder, add the data to the arrays
+for fnum, folder in enumerate(data_folders):
+    print "Doing folder ", fnum+1, "/", len(data_folders),
     print ""
-    print "Trainingset loaded!"
-else:
-    car_images = []
-    steering_angles = []
+    rows = []
+    with open(folder+'driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            rows.append(line)
 
-    # For every folder, add the data to the arrays
-    for fnum, folder in enumerate(data_folders):
-        print "Doing folder ", fnum+1, "/", len(data_folders),
-        print ""
-        rows = []
-        with open(folder+'driving_log.csv') as csvfile:
-            reader = csv.reader(csvfile)
-            for line in reader:
-                rows.append(line)
+    first = True
 
-        first = True
+    # Add a steering correction
+    steering_correction = 0.2
+    for j, row in enumerate(rows):
+        # Print the current finishing of the current folder
+        print "\r", j+1, "/", len(rows), "completed...",
+    
+        # Skip header row
+        if first:
+            first = False
+            continue
+            
+        # For every of the 3 generated images, do the data generate thing
+        for i in range(3):
+            steering_center = float(row[3])
 
-        # Add a steering correction
-        steering_correction = 0.2
-        for j, row in enumerate(rows):
-            # Print the current finishing of the current folder
-            print "\r (", j+1, "/", len(rows), ")", round((j+1)/len(rows)*100,2), "% completed...",
-        
-            # Skip header row
-            if first:
-                first = False
-                continue
-                
-            # For every of the 3 generated images, do the data generate thing
-            for i in range(3):
-                steering_center = float(row[3])
+            steering_left = steering_center + steering_correction
+            steering_right = steering_center - steering_correction
 
-                steering_left = steering_center + steering_correction
-                steering_right = steering_center - steering_correction
+            path = folder+"/IMG/"
+            img_loc = row[i].split('/')[-1]
+            img = cv2.imread(path + img_loc)
 
-                path = folder+"/IMG/"
-                img_loc = row[i].split('/')[-1]
-                img = cv2.imread(path + img_loc)
+            # Add images to array
+            car_images.append(img)
 
-                # Add images to array
-                car_images.append(img)
+            # Add flipped images to array
+            car_images.append(cv2.flip(img, 1))
+            
+            angle = 0
+            
+            if i == 0: 
+                angle = steering_center
+            elif i == 1: 
+                angle = steering_left
+            elif i == 2:
+                angle = steering_right
+            else:
+                raise Exception('Uhhhm?')
+            
+            # Add steering angle to array
+            steering_angles.append(angle)
 
-                # Add flipped images to array
-                car_images.append(cv2.flip(img, 1))
-                
-                angle = 0
-                
-                if i == 0: 
-                    angle = steering_center
-                elif i == 1: 
-                    angle = steering_left
-                elif i == 2:
-                    angle = steering_right
-                else:
-                    raise Exception('Uhhhm?')
-                
-                # Add steering angle to array
-                steering_angles.append(angle)
+            # Add flipped steering angle to array
+            steering_angles.append(-angle)
 
-                # Add flipped steering angle to array
-                steering_angles.append(-angle)
+# Generate np arrays
+X_train = np.array(car_images)
+y_train = np.array(steering_angles)
 
-    # Generate np arrays
-    X_train = np.array(car_images)
-    y_train = np.array(steering_angles)
-
-    # Save the arrays for reuse options
-    np.save(X_file, X_train)
-    np.save(y_file, y_train)
-
-    print ""
-    print "Done making training set!" 
+print ""
+print "Done making training set!" 
 
 # The model, based on https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf given in the lecture
 model = Sequential()
